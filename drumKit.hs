a6966987360d6fe5264597a70ca9a824ffe320ff
Dalton Lundy <lundy044@umn.edu>
2018-03-16 13:37:37 -0500

    fixed sound issue

--- drumKit.hs
+++ drumKit.hs
@@ -8,21 +8,22 @@ import Control.Concurrent.Async
 import Sound.ALUT
 import Control.Parallel 
 import Control.Concurrent
-main =        
-             playIO 
-             (InWindow "test" (1000,1000) (100,100) )
-             (makeColorI 255 255 255 255)
-             30
-             myWorld
-             (\w ->( return blank))
-             events
-             (\tm w -> return w) 
-             where 
-                myWorld = World.World { val = 0 }
-                events (EventMotion _) w = return w
-                events (EventResize _) w = return w
-                events (EventKey k s _ _) w
-                      | k == (Char 'w') && (s == Down)     = playKick *>  return w
-                      |otherwise                          = return w
+main = withProgNameAndArgs runALUT $ \progName args -> do run
+             where run =         
+                        playIO 
+                        (InWindow "test" (1000,1000) (100,100) )
+                        (makeColorI 255 255 255 255)
+                        30
+                        myWorld
+                        (\w ->( return blank))
+                        events
+                        (\tm w -> return w) 
+                        where 
+                           myWorld = World.World { val = 0 }
+                           events (EventMotion _) w = return w
+                           events (EventResize _) w = return w
+                           events (EventKey k s _ _) w
+                               | k == (Char 'w') && (s == Down)     = playKick *>  return w
+                               |otherwise                          = return w
         
           
--- sounds.hs
+++ sounds.hs
@@ -7,13 +7,19 @@ import Control.Monad
 import Control.Concurrent.Async
 import Control.Applicative
 import Control.Concurrent
-import Graphics.UI.SDL.Mixer
+import Sound.ALUT
+
+
 kickBuf =  "./Bass-Drum-1.wav"
 
-playSound file = do  SDL.init [SDL.InitAudio]
-                     openAudio 44100 AudioS16LSB 2 4096
-                     allocateChannels 16
-                     wave <- loadWAV file
-                     playChannel (-1) wave 0
+playSound :: String -> IO ()
+playSound file =  do buf <- createBuffer (File file);
+                     source <- genObjectName;
+                     buffer source $= Just buf;
+                     play [source];
+                       
+                     
+                     
+                     
 playKick =   
       (playSound kickBuf  )   
