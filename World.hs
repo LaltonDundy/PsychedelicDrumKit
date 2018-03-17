45e13dca8812b3d9a23467f623489cf27781cb1f
Dalton Lundy <lundy044@umn.edu>
2018-03-17 12:38:36 -0500

    Added first rendering

--- World.hs
+++ World.hs
@@ -1,8 +1,21 @@
 module World where
+import Graphics.Gloss.Data.Color
+import Graphics.Gloss.Data.Picture
+import Graphics.Gloss.Data.Display
+
+
+
 
 
 data World = World {
-    val :: Int      
+
+    kick :: Bool
+
     } deriving (Show, Eq) 
 
 
+render :: World -> IO Picture
+render w = if (not.kick $ w) then return Blank
+           else return (circle 150.0)
+
+ 
--- drumKit.hs
+++ drumKit.hs
@@ -7,23 +7,25 @@ import Sounds
 import Sound.ALUT
 
 
-
 main = withProgNameAndArgs runALUT $ \progName args -> do run --intitialize any state that needs be
+
              where run =         
                         playIO 
                         (InWindow "test" (1000,1000) (100,100) ) -- Display
                         (makeColorI 255 255 255 255)             -- Default Color (White)
                         30                                       -- Frames per second
                         myWorld                                  -- initial world state
-                        (\w ->( return blank))                   -- project/render world into IO
+                        World.render                             -- project/render world into IO
                         events                                   -- handle world state in regards to events
                         (\tm w -> return w)                      -- handle world state in regards to time
                         where 
-                           myWorld = World.World { val = 0 }
+                           myWorld = World.World { kick = False }
+
                            events (EventMotion _) w = return w
                            events (EventResize _) w = return w
                            events (EventKey k s _ _) w
-                               | k == (Char 'w') && (s == Down)     = playKick *>  return w
-                               |otherwise                          = return w
+                               | k == (Char 'w') && (s == Down)     = do playKick  
+                                                                         return w{kick = True}
+                               |otherwise                          = return w{kick = False}
         
           
