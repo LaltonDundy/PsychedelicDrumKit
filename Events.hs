73d12979b1fb568f197b4d4c70cf584cb6ac51f9
Dalton Lundy <lundy044@umn.edu>
2018-04-10 00:01:55 -0500

    updates

--- Events.hs
+++ Events.hs
@@ -16,22 +16,40 @@ events (EventKey k s _ _) w =
 
     case (state w) of
     
-    First  _ ->  keyBoard k
+    First  p ->  keyBoard k
                where keyBoard v 
-                       | v == (Char 'p')                     = return w {
-                                                                  isPaused = True,
-                                                                  state = Second $ World.Pause {resumePressed = False}       
-                                                                        }
+
+                       | v == (Char 'p')  = 
+
+                          return w {
+                             isPaused = True,
+                             state    = Second $ World.Pause {resumePressed = False}       
+                                    }
 
 
-                       | v == (Char 'w')                    = if  (s == Down) && (not.isPaused $ w ) then 
-                                                                  (playKick *> 
-                                                                   return w {
-                                                                   state = First $  World.Play {kick = True}
-                                                                             } ) 
-                                                              else return w { state = First $ World.Play {kick = False} } 
+                       | v == (Char 'w') =
 
-                       |otherwise                            = return w 
+                            if  (s == Down) then (
+                               playKick *> return w  {
+                               state = First $ p { kick = True } 
+                                                      }                 ) 
+
+                            else return w { state = First $ p { kick = False } }
+
+
+                       | v == (Char 'd') =
+
+                            if  (s == Down) then (
+                               playSnare *> return w  {
+                               state = First $ p { snare = True }
+                                                      }                 ) 
+
+                            else return w { state = First $ p {snare = False}}
+
+
+
+                       |otherwise         
+                            = return w 
 
     Second _ -> return w
                                                      
